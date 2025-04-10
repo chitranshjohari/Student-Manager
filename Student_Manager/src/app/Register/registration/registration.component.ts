36fac793fbeea58a6e24cc7687abb6441c0e513d
chitranshjohari <131401355+chitranshjohari@users.noreply.github.com>
2025-04-10 06:16:36 +0530

    Login and Registration UI

---
+++ Student_Manager/src/app/Register/registration/registration.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-registration',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent {
+  userForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      console.log('Form Submitted!', this.userForm.value);
+    } else {
+      this.userForm.markAllAsTouched();
+    }
+  }
+}
